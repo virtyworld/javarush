bcdc8bf56d185fdedef16617123c96bb346c3b5d
virtyworld <44694185+virtyworld@users.noreply.github.com>
2018-12-20 16:10:48 +0300

    Update Solution.java

--- 1.JavaSyntax/src/com/javarush/task/task01/task0131/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0131/Solution.java
@@ -1,16 +1,60 @@
-package com.javarush.task.task01.task0131;
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
-Полнометражная картина
+Парсер реквестов
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(getMetreFromCentimetre(243));
+    public static void main(String[] args)  {
+        //add your code here
+        Scanner scan = new Scanner(System.in);
+        String reader = scan.nextLine();
+        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        String[] out = reader.split("&");
+//        System.out.println(Arrays.toString(out));
+
+
+        Map<String, List<String>> params = new HashMap<String, List<String>>();
+        String[] urlParts = reader.split("\\?");
+        if (urlParts.length > 1) {
+            String query = urlParts[1];
+            for (String param : query.split("&")) {
+                String[] pair = param.split("=");
+                String key = URLDecoder.decode(pair[0], StandardCharsets.UTF_8);
+                System.out.println(key);
+                String value = "";
+                if (pair.length > 1) {
+                    value = URLDecoder.decode(pair[1], StandardCharsets.UTF_8);
+                }
+
+                List<String> values = params.get(key);
+
+                if (values == null) {
+                    values = new ArrayList<String>();
+                    params.put(key, values);
+                }
+                values.add(value);
+
+            }
+        }
+
     }
 
-    public static int getMetreFromCentimetre(int centimetre) {
-        //напишите тут ваш код
-        return  centimetre /100;
+    public static void alert(double value) {
+        System.out.println("double: " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String: " + value);
     }
 }
